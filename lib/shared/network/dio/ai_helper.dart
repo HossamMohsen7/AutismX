961ffd114411b6cc41e8fed9d77181f23c7339e9
Hossam Mohsen <6911267+HossamMohsen7@users.noreply.github.com>
2022-07-02 02:03:53 +0200

    Prediction

--- lib/shared/network/dio/ai_helper.dart
+++ lib/shared/network/dio/ai_helper.dart
@@ -1,19 +1,16 @@
 import 'dart:io';
 
 import 'package:autismx/shared/network/dio/dio_helper.dart';
-import 'package:autismx/shared/network/end_point.dart' as endpoints;
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class AIDioHelper {
-  ///Usage Example:
-  ///AIDioHelper.uploadImage(imageFile: File("./assets/image/after.png").absolute).then((response) {});
   static Future<Response<Map<String, dynamic>>> uploadImage({
     @required File imageFile,
   }) async {
     //TODO change url
     return await DioHelper.postData(
-        url: "https://509d-156-201-67-186.eu.ngrok.io/upload_image",
+        url: "https://c5f5-156-201-67-186.eu.ngrok.io/upload_image",
         data: FormData.fromMap({
           "file": await MultipartFile.fromFile(imageFile.path,
               filename: imageFile.path.split('/').last)
